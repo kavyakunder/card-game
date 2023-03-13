ec1f7daf50ea821958f9197c9e6a3f123c6bdd52
kavyakunder <kavya.k@deuexsolutions.com>
2023-03-13 15:11:05 +0530

    made changes

--- src/component/Card.test.tsx
+++ src/component/Card.test.tsx
@@ -1,36 +1,24 @@
 import { Card } from "./Card";
+
 import { fireEvent, render, screen } from "@testing-library/react";
 import { CardProps } from "./Card";
 
 describe("Card", () => {
-  it("should render card on screen", () => {
-    render(<Card {...mockProps} />);
+  it("Should render card on screen", () => {
+    const flippedCard = { ...mockCard, isFlipped: true };
+    render(<Card {...mockProps} card={flippedCard} />);
+
     const card = screen.getByTestId("card");
     expect(card).toBeInTheDocument();
   });
 
-  it("calls handleCardClick function when clicked", () => {
+  it("Should call handleCardClick function when clicked", () => {
     render(<Card {...mockProps} />);
 
     const card = screen.getByTestId("card");
     fireEvent.click(card);
 
     expect(mockHandleCardClick).toHaveBeenCalledWith(mockCard);
-  });
-
-  it("shows the front design when the card is flipped", () => {
-    const flippedCard = { ...mockCard, isFlipped: true };
-    render(<Card card={flippedCard} handleCardClick={mockHandleCardClick} />);
-
-    const card = screen.getByTestId("card");
-    expect(card.innerHTML).toContain(flippedCard.design);
-  });
-
-  it("shows the back design when the card is not flipped", () => {
-    render(<Card {...mockProps} />);
-
-    const card = screen.getByTestId("card");
-    expect(card.innerHTML).not.toContain(mockCard.design);
   });
 });
 
